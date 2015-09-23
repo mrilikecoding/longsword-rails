f44e4941c24bdbf79ef216a0edb8e0a7097432e8
Nathan Green <nathanrgreen@gmail.com>
2015-09-23 14:32:33 -0700

    dont bomb out if there are no weather records

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,27 @@
 class HomeController < ApplicationController
 
   def index
-    
+
     weather = WeatherCondition.last
+    @weather_available = WeatherCondition.count > 0
 
-    conditions = JSON.parse(weather.conditions)
-    forecast = JSON.parse(weather.forecast)
-    astronomy = JSON.parse(weather.astronomy)
+    if @weather_available
+      conditions = JSON.parse(weather.conditions)
+      forecast = JSON.parse(weather.forecast)
+      astronomy = JSON.parse(weather.astronomy)
 
-    @current_temp = conditions["current_observation"].present? ? conditions["current_observation"]["temp_f"] : 74.04
-    @current_weather = conditions["current_observation"].present? ? conditions["current_observation"]["weather"] : "clear"
-    @icon = conditions["current_observation"].present? ? conditions["current_observation"]["icon"] : "clear"
+      @current_temp = conditions["current_observation"].present? ? conditions["current_observation"]["temp_f"] : 74.04
+      @current_weather = conditions["current_observation"].present? ? conditions["current_observation"]["weather"] : "clear"
+      @icon = conditions["current_observation"].present? ? conditions["current_observation"]["icon"] : "clear"
 
-    @after_sunrise = astronomy["sun_phase"].present? ? (astronomy["sun_phase"]["sunrise"]["hour"].to_i > Time.now.hour) : false
-    @before_sunrise = astronomy["sun_phase"].present? ? (Time.now.hour <= astronomy["sun_phase"]["sunrise"]["hour"].to_i) : false
-    @after_close =  Time.now.hour >= 17 || Time.now.hour < 3
+      @after_sunrise = astronomy["sun_phase"].present? ? (astronomy["sun_phase"]["sunrise"]["hour"].to_i > Time.now.hour) : false
+      @before_sunrise = astronomy["sun_phase"].present? ? (Time.now.hour <= astronomy["sun_phase"]["sunrise"]["hour"].to_i) : false
+      @after_close =  Time.now.hour >= 17 || Time.now.hour < 3
 
-    @high = forecast["forecast"].present? ? forecast["forecast"]["simpleforecast"]["forecastday"][0]["high"]["fahrenheit"] : 74.04
-    @high_tomorrow = forecast["forecast"].present? ? forecast["forecast"]["simpleforecast"]["forecastday"][1]["high"]["fahrenheit"] : 74.04
-    @forecast = forecast["forecast"].present? ? forecast["forecast"]["simpleforecast"]["forecastday"][0]["conditions"] : "clear"
+      @high = forecast["forecast"].present? ? forecast["forecast"]["simpleforecast"]["forecastday"][0]["high"]["fahrenheit"] : 74.04
+      @high_tomorrow = forecast["forecast"].present? ? forecast["forecast"]["simpleforecast"]["forecastday"][1]["high"]["fahrenheit"] : 74.04
+      @forecast = forecast["forecast"].present? ? forecast["forecast"]["simpleforecast"]["forecastday"][0]["conditions"] : "clear"
+    end
 
 
     render "home/home"
